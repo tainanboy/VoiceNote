3296cebeab6577130afbf2012e2f3361095a29b7
jingwg <chelseagjw@hotmail.com>
2019-05-04 00:24:02 -0400

    Add tagify

--- public/js/script.js
+++ public/js/script.js
@@ -158,7 +158,7 @@ $('#analysis-note-btn').on('click', function(e) {
     //console.log("input is:"+input);
     //post to /nlp route: call goolge entites api
     var data = {text: input};
-    //
+
     var entity = $.ajax({
       type: 'POST',
       data: JSON.stringify(data),
@@ -177,8 +177,8 @@ $('#analysis-note-btn').on('click', function(e) {
       //console.log(res);
       //console.log(res['entities']);
       output = colorText(input,res);
+      AddTags(res);
       instructions.text('Analysis complete');
-      console.log(output);
     });
 })
 
@@ -207,8 +207,8 @@ function renderNotes(notes) {
     }
     notesList.html(html);
 }
-//show cat
 
+//show cat
 function showTooltip(x) {
 
   var popup = $(x);
@@ -256,6 +256,73 @@ function colorText(text,response){
   outputText.append(output_html)
   return words_arr
 }
+//add tags for the analysis
+function AddTags(response) {
+  //format : /Arts & Entertainment/Music & Audio/Pop Music
+  catgories = response['categories'][0]['name'].substring(1);
+  cat_list = catgories.split('/')
+  confidence  = response['categories'][0]
+  console.log(catgories);
+  console.log(cat_list);
+  tagName = cat_list.join(", ")
+  $('input[name=tags]').value = tagName
+  $('input[name=tags]').tagify();
+  // get tags from the server (ajax) and add them:
+  $('input[name=tags]').data('tagify').addTags(tagName);
+  //$('input[name=tags]').setAttribute( "type='hidden'")
+
+
+
+
+
+
+// "remove all tags" button event listener
+//document.querySelector('.tags--removeAllBtn').addEventListener('click', tagify.removeAllTags.bind(tagify))
+
+// Chainable event listeners
+/*tagify.on('add', onAddTag)
+      .on('remove', onRemoveTag)
+      .on('input', onInput)
+      .on('edit', onTagEdit)
+      .on('invalid', onInvalidTag)
+      .on('click', onTagClick);
+      */
+}
+
+// tag added callback
+function onAddTag(e){
+    console.log("onAddTag: ", e.detail);
+    console.log("original input value: ", input.value)
+    tagify.off('add', onAddTag) // exmaple of removing a custom Tagify event
+}
+
+// tag remvoed callback
+function onRemoveTag(e){
+    console.log(e.detail);
+    console.log("tagify instance value:", tagify.value)
+}
+
+// on character(s) added/removed (user is typing/deleting)
+function onInput(e){
+    console.log(e.detail);
+    console.log("onInput: ", e.detail);
+}
+
+function onTagEdit(e){
+    console.log("onTagEdit: ", e.detail);
+}
+
+// invalid tag added callback
+function onInvalidTag(e){
+    console.log("onInvalidTag: ", e.detail);
+}
+
+// invalid tag added callback
+function onTagClick(e){
+    console.log(e.detail);
+    console.log("onTagClick: ", e.detail);
+}
+
 
 function saveNote(dateTime, content) {
     localStorage.setItem('note-' + dateTime, content);
